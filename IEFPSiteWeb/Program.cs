81bc991fb672fd1cfd71a03c93e14b538c08bfe6
isidro moreira <isidromoreira@hotmail.com>
2023-02-09 17:29:22 +0000

    4aFinal

--- IEFPSiteWeb/Program.cs
+++ IEFPSiteWeb/Program.cs
@@ -1,7 +1,15 @@
+using IEFPSiteWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//DBContext
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("IEFPSiteDb")));
 
 var app = builder.Build();
 
