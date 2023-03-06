3d07db76e69911ce900f7b601fe1de3c8b6607a9
isidro moreira <isidromoreira@hotmail.com>
2023-03-06 16:37:38 +0000

    Butons delete edit

--- IEFPSiteWeb/Controllers/CategoryController.cs
+++ IEFPSiteWeb/Controllers/CategoryController.cs
@@ -32,7 +32,7 @@ namespace IEFPSiteWeb.Controllers
         {
             if(obj.Name == obj.DisplayOrder.ToString())
             {
-                ModelState.AddModelError("CustomerError", "The Display Order cannot match the Name Field."); /*Adicionar erro ao modelo para ser invalido*/
+                ModelState.AddModelError("Name", "The Display Order cannot match the Name Field."); /*Adicionar erro ao modelo para ser invalido*/
             }
 
             if (ModelState.IsValid)
@@ -40,6 +40,84 @@ namespace IEFPSiteWeb.Controllers
             _db.Categories.Add(obj);
             _db.SaveChanges();
             return RedirectToAction("Index"); //("Index", "Home") para ir ao controlador home
+            }
+
+            return View(obj);
+        }
+
+        //GET Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id==0) {
+                return NotFound();
+            }
+
+            //Pesquisa a base dados exemplos
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id == id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u=>u.Id == id);
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The Display Order cannot match the Name Field."); /*Adicionar erro ao modelo para ser invalido*/
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index"); //("Index", "Home") para ir ao controlador home
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //Pesquisa a base dados exemplos
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id == id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u=>u.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category obj)
+        {
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("Name", "The Display Order cannot match the Name Field."); /*Adicionar erro ao modelo para ser invalido*/
+            //}
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index"); //("Index", "Home") para ir ao controlador home
             }
 
             return View(obj);
--- IEFPSiteWeb/Models/Category.cs
+++ IEFPSiteWeb/Models/Category.cs
@@ -12,6 +12,7 @@ namespace IEFPSiteWeb.Models
         public string Name { get; set; } = null!;
 
         [Display(Name = "Display Order")] /*Mudar mensagem de Validação*/
+        [Range(1,100, ErrorMessage ="Display order must be betwen 1 and 100")]
         public int DisplayOrder { get; set; }
 
 
